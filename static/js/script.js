deabecf926811268dbeb20fc1ce91dd05b8d374d
Sukka Vignesh Goud <sukkavigneshgoud@gmail.com>
2026-06-10 08:10:47 +0530

    editing done

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,37 @@
+/* LOADER */
+
+window.addEventListener("load", ()=>{
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(()=>{
+
+        loader.classList.add("loader-hide");
+
+    },3000);
+
+});
+
 /* OPEN BUTTON */
 
 const button = document.querySelector(".open-btn");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", ()=>{
 
     window.scrollTo({
-        top: window.innerHeight,
-        behavior: "smooth"
+
+        top:window.innerHeight,
+
+        behavior:"smooth"
     });
 
 });
-
 
 /* COUNTDOWN */
 
 const weddingDate = new Date(2026, 6, 5, 0, 0, 0).getTime();
 
-function updateCountdown() {
+function updateCountdown(){
 
     const now = new Date().getTime();
 
@@ -40,11 +55,55 @@ function updateCountdown() {
     );
 
     document.getElementById("days").innerHTML = days;
+
     document.getElementById("hours").innerHTML = hours;
+
     document.getElementById("minutes").innerHTML = minutes;
+
     document.getElementById("seconds").innerHTML = seconds;
 }
 
 updateCountdown();
 
 setInterval(updateCountdown, 1000);
+
+/* POPUP GALLERY */
+
+const popup = document.getElementById("imagePopup");
+
+const popupImg = document.getElementById("popupImg");
+
+const closePopup = document.getElementById("closePopup");
+
+const galleryImages = document.querySelectorAll(".gallery-item img");
+
+/* OPEN */
+
+galleryImages.forEach((img)=>{
+
+    img.addEventListener("click", ()=>{
+
+        popup.style.display = "flex";
+
+        popupImg.src = img.src;
+    });
+
+});
+
+/* CLOSE BUTTON */
+
+closePopup.addEventListener("click", ()=>{
+
+    popup.style.display = "none";
+});
+
+/* OUTSIDE CLICK */
+
+popup.addEventListener("click", (e)=>{
+
+    if(e.target === popup){
+
+        popup.style.display = "none";
+    }
+
+});
